708d1db86cf7a17d538370c450cca462dfe1ada6
ChouguleAnkita13 <flutterankita@gmail.com>
2024-11-12 12:03:10 +0530

    splash screen

---
+++ Firebase/authentication_example/lib/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'package:authentication_example/home_screen.dart';
+import 'package:authentication_example/login.dart';
+import 'package:authentication_example/session_data_shared_pre.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  void navigateToScreen(BuildContext context) {
+    Future.delayed(const Duration(seconds: 3), () async {
+      /// SHARED PREFERENCE - Get Data
+      await SessionData.getSessionData();
+      print("IS LOGIN : ${SessionData.isLogin!}");
+      if (SessionData.isLogin!) {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(
+              email: SessionData.email!,
+            ),
+          ),
+        );
+      } else {
+        Navigator.of(context).pushReplacement(
+          MaterialPageRoute(
+            builder: (context) => const LoginScreen(),
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    navigateToScreen(context);
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Welcome",
+          style: TextStyle(fontSize: 50),
+        ),
+      ),
+    );
+  }
+}
