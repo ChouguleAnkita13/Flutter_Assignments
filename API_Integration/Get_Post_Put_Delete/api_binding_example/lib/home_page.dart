ef5742f5a156fac048b06f9da7b376199d3474d9
ChouguleAnkita13 <flutterankita@gmail.com>
2024-11-14 16:44:04 +0530

    get api data on ui side

--- API_Integration/Get_Post_Put_Delete/api_binding_example/lib/home_page.dart
+++ API_Integration/Get_Post_Put_Delete/api_binding_example/lib/home_page.dart
@@ -1,33 +1,73 @@
 import 'dart:developer';
-
 import 'package:api_binding_example/api_controller.dart';
+import 'package:api_binding_example/todo_model.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Todos> todoList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.pink[50],
       appBar: AppBar(
         title: const Text("API Binding"),
         centerTitle: true,
         backgroundColor: Colors.pink[100],
       ),
-      body: Center(
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
-            ElevatedButton(
-                onPressed: () async {
-                  Map todo = await ApiController.getDataFromApi();
-                  List todoList = todo["todos"];
-                  log("$todoList");
-                },
-                child: Text("Get Data")),
-            ElevatedButton(onPressed: () {}, child: const Text("Post Data")),
-            ElevatedButton(onPressed: () {}, child: const Text("Delete Data")),
-            ElevatedButton(onPressed: () {}, child: const Text("Update Data")),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                    onPressed: () async {
+                      ///GET DATA FROM API PASSED FROM APICONTROLLER.GETDATAFROMAPI FUNCTION
+                      ///
+                      TodoModel todoObj = await ApiController.getDataFromApi();
+
+                      ///STORE LIST  IN LOCAL TODOLIST
+                      todoList = todoObj.todosList!;
+                      setState(() {});
+                      log("${todoList[0].todo}");
+                    },
+                    child: const Text("Get Data")),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Post Data")),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Delete Data")),
+                ElevatedButton(
+                    onPressed: () {}, child: const Text("Update Data")),
+              ],
+            ),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: todoList.length,
+                    itemBuilder: (context, index) => Padding(
+                          padding: const EdgeInsets.only(top: 10),
+                          child: ListTile(
+                            tileColor: Colors.amber[100],
+                            contentPadding: const EdgeInsets.all(10),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            leading: Text("${todoList[index].id}"),
+                            title: Text("${todoList[index].todo}"),
+                            subtitle: Text("${todoList[index].userId}"),
+                            trailing: Checkbox(
+                                value: todoList[index].completed,
+                                activeColor: Colors.green,
+                                onChanged: (value) {}),
+                          ),
+                        )))
           ],
         ),
       ),
