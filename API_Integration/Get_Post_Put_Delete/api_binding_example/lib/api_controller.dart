f06177e1f1fc6308d4cd6c22dc2b16fc4913edcf
ChouguleAnkita13 <flutterankita@gmail.com>
2024-11-15 10:36:21 +0530

    added try catch

--- API_Integration/Get_Post_Put_Delete/api_binding_example/lib/api_controller.dart
+++ API_Integration/Get_Post_Put_Delete/api_binding_example/lib/api_controller.dart
@@ -1,28 +1,41 @@
 import 'dart:convert';
-// import 'dart:developer';
-
+import 'dart:developer';
 import 'package:api_binding_example/todo_model.dart';
 import 'package:http/http.dart' as http;
 
 class ApiController {
   static Future<TodoModel> getDataFromApi() async {
-    ///HTTP.GET(URL) REQUIRES URL AND OUR LINK IS IN STRING FORMAT
-    ///SO WE PARSE THAT STRING IN URL
+    try {
+      ///HTTP.GET(URL) REQUIRES URL AND OUR LINK IS IN STRING FORMAT
+      ///SO WE PARSE THAT STRING IN URL
+      /// PARSE THE STRING URL INTO URI FORMAT FOR HTTP GET REQUEST
+      Uri url = Uri.parse("https://dummyjson.com/todos");
+      http.Response response = await http.get(url);
 
-    Uri url = Uri.parse("https://dummyjson.com/todos");
-    http.Response response = await http.get(url);
+      /// CHECK IF THE REQUEST WAS SUCCESSFUL
+      if (response.statusCode == 200) {
+        ///MAIN DATA IS ALWAYS IN RESPONSE.BODY WHICH IS IN STRING FORMAT
+        ///TO COVERT THAT STRING IN TO JSON FORMAT (I.E MAP:{KEY:VALUE} OR LIST OF MAP) JSON.DECODE(RESPONSE.BODY) USED
+        ///WHICH CONVERTS STRING INTO JSON OBJECT
 
-    ///MAIN DATA IS ALWAYS IN RESPONSE.BODY WHICH IS IN STRING FORMAT
-    ///TO COVERT THAT STRING IN TO JSON FORMAT (I.E MAP:{KEY:VALUE} OR LIST OF MAP)
-    ///JSON.DECODE(RESPONSE.BODY) USED
-    ///WHICH CONVVERTS STRING INTO JSON OBJECT
-    // log(response.body);
+        // log(response.body);
 
-    Map<String, dynamic> responseData = json.decode(response.body);
-    // log("${responseData["total"]}");
+        /// CONVERT RESPONSE.BODY (STRING) INTO JSON FORMAT
+        Map<String, dynamic> responseData = json.decode(response.body);
 
-    ///COVERT THE RESPONSE DATA MAP INTO MODEL CLASS
-    TodoModel todoModel = TodoModel(responseData);
-    return todoModel;
+        /// CONVERT THE JSON RESPONSE MAP INTO A TODOMODEL INSTANCE
+        TodoModel todoModel = TodoModel(responseData);
+        return todoModel;
+      } else {
+        /// THROW AN EXCEPTION IF STATUS CODE IS NOT SUCCESSFUL (200)
+        throw Exception("Failed to load data: ${response.statusCode}");
+      }
+    } catch (error) {
+      /// LOG THE ERROR IF AN EXCEPTION OCCURS
+      log("Error fetching data: $error");
+
+      /// RETURN A DEFAULT OR EMPTY TODOMODEL IN CASE OF AN ERROR
+      return TodoModel({});
+    }
   }
 }
