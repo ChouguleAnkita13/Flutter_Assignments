070bad10b646cf246eaf8bf6dfe9e5ef1a7bc37c
ChouguleAnkita13 <flutterankita@gmail.com>
2024-12-11 16:17:53 +0530

    comments added

--- widget_lifecycle/lib/widget_lifecycle.dart
+++ widget_lifecycle/lib/widget_lifecycle.dart
@@ -7,17 +7,28 @@ import 'package:widget_lifecycle/text_screen.dart';
 class WidgetLifecycle extends StatefulWidget {
   const WidgetLifecycle({super.key});
 
+  ///THIS METHOD GET CALLED WHEN OBJECT OF STATEFUL WIDGET IS CALLED
+  ///THIS METHOD RETURNS OBJECT OF ITS STATE CLASS
   @override
-  State<WidgetLifecycle> createState() => _WidgetLifecycleState();
+  State<WidgetLifecycle> createState() {
+    log("In Create state");
+    return _WidgetLifecycleState();
+  }
 }
 
 class _WidgetLifecycleState extends State<WidgetLifecycle> {
+  ///THIS METHOD CALLED WHEN STATE OBJECT IS CREATED
+  ///THIS CALLS ONLY ONCE WHEN STATE OBJECT IS CREATED
+  ///IT CAN CONTAIN FETCHING DATA FROM EXTERNAL RESOURCES ,INITIALIZING VARIABLES
+
   @override
   void initState() {
     super.initState();
     log("In InitState");
   }
 
+  ///THIS METHOD CALLED IMIDIATLY AFTER INITSTATE
+  ///
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -25,6 +36,9 @@ class _WidgetLifecycleState extends State<WidgetLifecycle> {
   }
 
   String str = "Ankita";
+
+  ///THIS BUILD METHOD GET CALLED WHEN CHANGE HAPPENS IN WIDGET TREE
+  ///THE FIRST CALL GOES TO BUILD METHOD FROM FRAMEWORK.DART
   @override
   Widget build(BuildContext context) {
     log("In build method");
@@ -37,6 +51,7 @@ class _WidgetLifecycleState extends State<WidgetLifecycle> {
           ),
           ElevatedButton(
             onPressed: () {
+              ///THIS SETSTATE MENTHOD NOTIFIES FREAMWORK THAT CHANGE HAS HAPPENED AND REQUIRES TO RERUN THE BUILD METHOD
               setState(() {
                 log("In setState method");
 
@@ -57,6 +72,10 @@ class _WidgetLifecycleState extends State<WidgetLifecycle> {
     );
   }
 
+  ///DEACTIVATE AND DISPOSE METHOD WORKS WHEN WIDGET REMOVE FROM
+  ///WIDGET TREE PERMANATLY
+  ///THIS DISPOSE METHOD CALLS ONLY ONCE WHEN WIDGET REMOVE PERMANANTLY FROM WIDGET TREE
+  ///E.G:THE CURRENT WIDGET IS REMOVED PERMANtly WHEN IT REPLACED WITH NEXTPAGE WIDGET
   @override
   void deactivate() {
     super.deactivate();
